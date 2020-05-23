7d6a01d40ed53e42ad8280670f64d6c13e0c1e5f
Cataldo Malerba <aldomalerba.dev@gmail.com>
2020-05-23 19:16:00 +0200

    Preview

---
+++ README.md
@@ -0,0 +1,22 @@
+# square-post-custom-element
+A simple square custom element
+
+## Preview
+
+![Screenshot](preview/screenshot.png)
+
+## Usage
+
+```html
+<script src="square.js"></script>
+
+...
+
+<square-post
+    avatar="avatar.png"
+    width="40%"
+    userName="User Name"
+    text="This is a simple sample text."
+    backgroundColor="#008080"
+    textColor="#ffffff"></square-post>
+```
--- square.js
+++ square.js
@@ -1,20 +1,22 @@
 (function () {
   const template = document.createElement("template");
   template.innerHTML = `
-<div class="square">               
-  <div class="content">
-    <div class="squareHeader">
-        <img class="squarePicture" alt="...">
-        <span class="squareUsername"></span>                   
-    </div>
-    <div class="squareBody">
-      <div class="content">
-        <span class="text-inner"></span>
+
+  <div class="square">
+  <div class="wrapper">
+      <div class="squareHeader">
+          <img class="squarePicture" alt="...">
+          <span class="squareUsername"></span>
+      </div>            
+      <div class="squareBody">
+          <div class="text-inner">
+              
+          </div>
       </div>
-    </div>
-    <div class="squareFooter">          
-    </div>
-  </div>
+      <div class="squareFooter">
+
+      </div>
+  </div>      
 </div>`;
 
   class Square extends HTMLElement {
@@ -34,12 +36,20 @@
     }
 
     static get observedAttributes() {
-      return ["width", "text", "avatar", "userName"];
+      return [
+        "width",
+        "text",
+        "avatar",
+        "userName",
+        "backgroundColor",
+        "textColor"
+      ];
     }
 
     attributeChangedCallback(name, oldValue, newValue) {
       switch (name) {
         case "avatar":
+          debugger;
           updateAvatar(this);
         case "userName":
             updateUserName(this);
@@ -94,85 +104,94 @@
 
   function updateStyle(elem) {
     let width = elem.getAttribute("width") || "100%";
+    let backgroundColor = elem.getAttribute("backgroundColor") || ""; 
+    let textColor = elem.getAttribute("textColor") || ""; 
     const shadow = elem.shadowRoot;
     shadow.querySelector("style").textContent = `
     .square{
-        position: relative;
-        width: ${width};
-        border-style: solid;
-        border-width: 1px;
-        background-color: teal;
-        padding: 50px:
-      }
-      
-      .square::before{
-        content: '';
-        display: block;
-        padding-top: 100%;
-      }
-      
-      .squareHeader,
-      .squareFooter {
-          display: inline-block;
-          position: relative;
-          height: 15%;
-      }
-
-      /* Fake (pseudo) element, that enables vertical-align */
-      .squareHeader:before {
-        content: "";
-        display: inline-block;
-        vertical-align: middle;
-        height: 100%;
-      }
-      
-      .squarePicture{
-        vertical-align: middle;
-        height: 80%;
-        max-width: 100%;
-        border-radius: 100%;
-        padding-left: 10px
-      }
-
-      .squareUsername{
-        vertical-align: middle;
-        padding-left: 20px;
-        font-size: 1.5em;
-      }
-      
-      .squareBody {
-          position: relative;
-          height: 70%;
-      }
-      
-      .content{
-        position: absolute;
-        top: 0; left:0;
-        height: 100%;
-        width: 100%;
-      }
-
-      .text-inner {
-        position: relative;
-        top: 50%;
-        transform: translateY(-50%);
-        overflow-wrap: break-word;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        text-align: left;
-        -webkit-line-clamp: 6;
-        -webkit-box-orient: vertical;
-        white-space: pre-line;
-        font-family: sans-serif;
-        font-size: 1.5em;
-      }
-      
-      .text-inner-footer {
-        position: relative;
-        top: 50%;
-        transform: translateY(-50%);
-      }
+      position: relative;
+      width: ${width || '100%'};
+      border-style: solid;
+      border-width: 1px;
+      background-color: ${backgroundColor || ""};
+      font-family: 'Open Sans', sans-serif;
+      letter-spacing: 0.2em;
+    }
+    
+    .square::before{
+      content: '';
+      display: block;
+      padding-top: 100%;
+    }
+  
+    .wrapper{
+      position: absolute;
+      top: 0; left:0;
+      height: 100%;
+      width: 100%;
+    }
+  
+    .squareHeader,
+    .squareFooter,
+    .squareBody{
+      position: absolute;
+      width: 100%;
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
+      -ms-flex-align: center;
+      -webkit-align-items: center;
+      -webkit-box-align: center;
+      align-items: center;
+    }
+    
+    .squareHeader,
+    .squareFooter{  
+      height: 15%;
+    }
+  
+    .squareHeader{
+      top: 0;
+    }
+  
+    .squareFooter{
+      bottom: 0;
+    }
+  
+    .squareBody{
+      top: 15%;
+      height: 70%;
+    }
+  
+    .squarePicture{
+      margin-left: 2.5%;
+      height: 80%;
+      max-width: 100%;
+      border-radius: 100%;
+    }
+  
+    .squareUsername{
+      margin-left: 2.5%;
+      vertical-align: middle;
+      padding-left: 10px;
+      font-size: 1.5rem;
+    }
+  
+    .text-inner {
+      margin-left: 2.5%;
+      margin-right: 2.5%;
+      overflow-wrap: break-word;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      text-align: left;
+      -webkit-line-clamp: 6;
+      -webkit-box-orient: vertical;
+      white-space: pre-line;
+      font-size: 20px;
+      line-height: 1.4em;
+      color: ${textColor || ""};
+    }  
     `;
   }
 
